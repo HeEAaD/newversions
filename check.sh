d68a58ad484bb05a84af399326dc2fb390b0006a
Steffen Matthischke <steffen.matthischke@gmail.com>
2014-05-14 14:01:37 +0200

    check.sh: read PROWL_API_KEY from file, which is assumed unchanged

--- check.sh
+++ check.sh
@@ -3,7 +3,7 @@
 VERSIONS_FILE='versions.csv'
 TMP_VERSIONS_FILE='/tmp/newversions.tmp'
 PLUGIN_DIR='plugins'
-PROWL_API_KEY='<Your Prowl API key here>'
+PROWL_API_KEY_FILE='prowl_api_key.conf'
 
 function inform_user () {
 	local name=$1
@@ -11,13 +11,24 @@ function inform_user () {
 	curl -s -d "apikey=$PROWL_API_KEY&application=New Version&event=$name has new version: $version" https://api.prowlapp.com/publicapi/add > /dev/null
 }
 
+PROWL_API_KEY=$(cat $PROWL_API_KEY_FILE)
+
+if [[ -z "$PROWL_API_KEY" ]] || [[ "$PROWL_API_KEY" == "<Your Prowl API key here>" ]]; then
+	echo "Set your Prowl API key in $PROWL_API_KEY_FILE."
+	exit 1
+fi
+
 for plugin in $PLUGIN_DIR/*.nvplugin; do
 
 	name=$(basename $plugin | rev | cut -c 10- | rev)
 	version=$($plugin)
 
+	if [[ -z "$version" ]]; then
+		continue
+	fi
+
 	if [[ -e "$VERSIONS_FILE" ]]; then
-		oldversion=$(grep "^$name," "$VERSIONS_FILE" | cut -d ',' -f 2 | head -n1 )
+		oldversion=$(grep "^$name," "$VERSIONS_FILE" | cut -d ',' -f 2- | head -n1 )
 		if [[ -z $oldversion ]]; then
 			echo "$name,$version" >> "$VERSIONS_FILE"
 		elif [[ $oldversion != $version ]]; then
